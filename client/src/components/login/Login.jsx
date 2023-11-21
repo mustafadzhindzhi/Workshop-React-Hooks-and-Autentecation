e13632a7df03d2630f66540441dffc185862c22e
mustafadzhindzhi <122883380+mustafadzhindzhi@users.noreply.github.com>
2023-11-21 11:13:58 +0200

    React Hooks and Authenticaiton

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,22 +1,49 @@
-export default function Login() {
-    return (
-        <section id="login-page" className="auth">
-            <form id="login">
+import useForm from "../../hooks/useForm.js";
 
-                <div className="container">
-                    <div className="brand-logo"></div>
-                    <h1>Login</h1>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+const loginFormKeys = {
+  Email: "email",
+  Password: "password",
+};
 
-                    <label htmlFor="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password" />
-                    <input type="submit" className="btn submit" value="Login" />
-                    <p className="field">
-                        <span>If you don't have profile click <a href="#">here</a></span>
-                    </p>
-                </div>
-            </form>
-        </section>
-    )
+export default function Login({ loginSubmitHandler }) {
+  const { values, onChange, onSubmit } = useForm(loginSubmitHandler, {
+    [loginFormKeys.Email]: "",
+    [loginFormKeys.Password]: "",
+  }); // we will take them from useForm
+  //name hase to be eqaul with the key
+
+  return (
+    <section id="login-page" className="auth">
+      <form id="login" onSubmit={onSubmit}>
+        <div className="container">
+          <div className="brand-logo"></div>
+          <h1>Login</h1>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name={loginFormKeys.Email}
+            placeholder="Sokka@gmail.com"
+            onChange={onChange}
+            value={values[loginFormKeys.Email]}
+          />
+
+          <label htmlFor="login-pass">Password:</label>
+          <input
+            type="password"
+            id="login-password"
+            name={loginFormKeys.Password}
+            onChange={onChange}
+            value={values[loginFormKeys.Password]}
+          />
+          <input type="submit" className="btn submit" value="Login" />
+          <p className="field">
+            <span>
+              If you don't have profile click <a href="#">here</a>
+            </span>
+          </p>
+        </div>
+      </form>
+    </section>
+  );
 }
