22742c76b0103cdede2cccac253007be0f9b0150
mustafadzhindzhi <122883380+mustafadzhindzhi@users.noreply.github.com>
2023-11-21 19:18:55 +0200

    React Hooks and Authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,31 +10,48 @@ import Home from "./components/home/Home.jsx";
 import GameList from "./components/game-list/GameList.jsx";
 import GameCreate from "./components/game-create/GameCreate.jsx";
 import Login from "./components/login/Login.jsx";
+import Logout from "./components/logout/Logout.jsx";
 import Register from "./components/register/Register.jsx";
 import GameDetails from "./components/game-details/GameDetails.jsx";
 
 function App() {
   const navigate= useNavigate();
-  const [auth, setAuth] = useState({});
+  const [auth, setAuth] = useState(() => {
+    localStorage.removeItem('accessToken');
+
+    return {};
+  });
 
   const loginSubmitHandler = async (values) => {
     const result = await authService.login(values.email, values.password);
 
     setAuth(result);
+    localStorage.setItem('accessToken', result.accessToken); 
 
     navigate(Path.Home);
   };
 
   const registerSubmitHandler = async(values) => {
-    console.log(values);
-  } 
+    const result = await authService.register(values.email, values.password);
+
+    setAuth(result);
+    localStorage.setItem('accessToken', result.accessToken); 
+
+    navigate(Path.Home);
+  };
+
+  const logoutHandler = () => {
+    setAuth({});
+    localStorage.removeItem('accessToken');
+  }
 
   const values = {
     loginSubmitHandler,
     registerSubmitHandler,
-    username: auth.username,
+    logoutHandler,
+    username: auth.username || auth.email,
     email: auth.email,
-    isAuthenticated: !!auth.username
+    isAuthenticated: !!auth.accessToken
   }
 
   return (
@@ -48,6 +65,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/games/:gameId" element={<GameDetails />} />
+          <Route path={Path.Logout} element={<Logout />} />
         </Routes>
       </div>
     </AuthContext.Provider>
--- client/src/lib/request.js
+++ client/src/lib/request.js
@@ -6,6 +6,15 @@ const buildOptions = (data) => {
         options.headers = {
             'content-type': 'application/json'
         };
+    } 
+
+    const token = localStorage.getItem('accessToken');
+
+    if(token) {
+        options.headers = {
+            ...options.headers,
+            'X-Authorization': token
+        }
     }
 
     return options;
@@ -17,7 +26,15 @@ const request = async (method, url, data) => {
         method,
     });
 
+    if (response.status === 204) {
+        return {};
+    }
+
     const result = await response.json();
+
+    if (!response.ok) {
+        throw result;
+    }
 
     return result;
 };
