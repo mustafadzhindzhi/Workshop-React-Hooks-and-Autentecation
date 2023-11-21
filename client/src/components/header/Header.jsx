65e4accf3ed24be9a76cebc978a2773cfe5814e6
mustafadzhindzhi <122883380+mustafadzhindzhi@users.noreply.github.com>
2023-11-21 18:14:53 +0200

    React Hooks and Authentication

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,19 +1,30 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom" // сменяме всички <а с Link и всички href ги сменяме с to
+import AuthContext from "../../contexts/authContext.js"
 
 export default function Header() { 
+    const {
+        isAuthenticated, 
+        username
+    } = useContext(AuthContext);
+
     return (
         <header>
         <h1><Link className="home" to="/">GamesPlay</Link></h1>
         <nav>
             <Link to="/games">All games</Link>
-            <div id="user">
+            {isAuthenticated && (<div id="user">
                 <Link to="/games/create">Create Game</Link>
                 <Link to="/logout">Logout</Link>
-            </div>
-            <div id="guest">
+                <span>| {username}</span>
+            </div>)}
+
+            {!isAuthenticated && (
+                <div id="guest">
                 <Link to="/login">Login</Link>
                 <Link to="/register">Register</Link>
             </div>
+            )}
         </nav>
     </header>
     )
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,27 +1,66 @@
+import { useContext } from "react";
+import AuthContext from "../../contexts/authContext.js";
+import useForm from "../../hooks/useForm.js";
+
+const registerFormKeys = {
+  Email: "email",
+  Password: "password",
+  ConfirmPassword: "confirm-password",
+};
+
 export default function Register() {
-    return (
-        <section id="register-page" className="content auth">
-            <form id="register">
-                <div className="container">
-                    <div className="brand-logo"></div>
-                    <h1>Register</h1>
+  const { registerSubmitHandler } = useContext(AuthContext);
+  const { values, onChange, onSubmit } = useForm(registerSubmitHandler, {
+    [registerFormKeys.Email]: "",
+    [registerFormKeys.Password]: "",
+    [registerFormKeys.ConfirmPassword]: "",
+  });
 
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="maria@email.com" />
+  return (
+    <section id="register-page" className="content auth">
+      <form id="register" onSubmit={onSubmit}>
+        <div className="container">
+          <div className="brand-logo"></div>
+          <h1>Register</h1>
 
-                    <label htmlFor="pass">Password:</label>
-                    <input type="password" name="password" id="register-password" />
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="maria@email.com"
+            onChange={onChange}
+            values={values[registerFormKeys.Email]}
 
-                    <label htmlFor="con-pass">Confirm Password:</label>
-                    <input type="password" name="confirm-password" id="confirm-password" />
+          />
 
-                    <input className="btn submit" type="submit" value="Register" />
+          <label htmlFor="pass">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="register-password"
+            onChange={onChange}
+            values={values[registerFormKeys.Password]}
+          />
 
-                    <p className="field">
-                        <span>If you already have profile click <a href="#">here</a></span>
-                    </p>
-                </div>
-            </form>
-        </section>
-    )
+          <label htmlFor="con-pass">Confirm Password:</label>
+          <input
+            type="password"
+            name="confirm-password"
+            id="confirm-password"
+            onChange={onChange}
+            values={values[registerFormKeys.ConfirmPassword]}
+          />
+
+          <input className="btn submit" type="submit" value="Register" />
+
+          <p className="field">
+            <span>
+              If you already have profile click <a href="#">here</a>
+            </span>
+          </p>
+        </div>
+      </form>
+    </section>
+  );
 }
